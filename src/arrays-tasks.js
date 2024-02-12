506de931923f165f0b7488aeb6954d5e59a416c9
Aliaksandra <a.sinitsyna07@gmail.com>
2024-02-12 14:14:12 +0100

    feat: add solution to task distinct

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -243,8 +243,10 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const uniqueSet = new Set(arr);
+  const uniqueArray = Array.from(uniqueSet);
+  return uniqueArray;
 }
 
 /**
