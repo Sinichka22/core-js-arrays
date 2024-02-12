32105a3f614f55c6246b780757dbfd63f8e404c3
Aliaksandra <a.sinitsyna07@gmail.com>
2024-02-12 12:40:08 +0100

    feat: add solution to task #6

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -135,8 +135,9 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const firstStringLength = arr[0].length;
+  return arr.every((str) => str.length === firstStringLength);
 }
 
 /**
